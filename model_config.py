7eb430bfb6d61099d960141990f95ef5b334c53e
Panepo <whisper.ko@gmail.com>
2025-01-08 09:33:35 +0800

    update function
    update function

--- model_config.py
+++ model_config.py
@@ -92,6 +92,8 @@ elif model == "phi3.5":
     Context: {context}
     Answer: <|end|>
     <|assistant|>""",
+    "console_message_start": "<|user|>\n",
+    "console_message_end": "<|end|> \n<|assistant|>\n",
     "completion_to_prompt": phi_completion_to_prompt,
   }
 else:
